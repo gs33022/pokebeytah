de143129eb1c430c6754b4ea23d04684bc35c0b6
U-GABENNIENIE2023\willy <willyfudgehj@gmail.com>
2024-07-05 01:10:53 -0500

    beta Viridian accessible, but still needs connections

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -1,15 +1,15 @@
 Route1WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  5, BULBASAUR
-	db  5, CHARMANDER
-	db  5, SQUIRTLE
-	db  5, BULBASAUR
-	db  5, CHARMANDER
-	db  5, SQUIRTLE
-	db  5, BULBASAUR
-	db  5, CHARMANDER
-	db  5, SQUIRTLE
-	db  5, BULBASAUR
+	db  2, BULBASAUR
+	db  4, CHARMANDER
+	db  3, SQUIRTLE
+	db  4, BULBASAUR
+	db  4, CHARMANDER
+	db  2, SQUIRTLE
+	db  3, BULBASAUR
+	db  2, CHARMANDER
+	db  2, SQUIRTLE
+	db  3, BULBASAUR
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
